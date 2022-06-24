ea8ae6985be42ad6af182dd83192701f9ce36317
noob-coder007 <69330216+noob-coder007@users.noreply.github.com>
2022-06-24 12:22:35 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.write("name:Obote Onowareke")
+document.write("<br>");
+document.write("height: 5.9ft")
+document.write("<br>");
+document.write("country: Nigeria")
+
+
+var Height = ' Height: 5.9ft,'
+var Country = ' Country: Nigeria.'
+window.alert('Name: Obote Onowareke,' + Height + Country);
